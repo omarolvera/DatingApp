3a3712234880fac157a49297049cc9c3b7199fbc
omar <omarolvera.rod@gmail.com>
2020-09-07 20:07:31 -0500

    initial commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        //TODO: create datacontext inject from Nuget Microsoft.EntityFramework.core
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+     
+        public DbSet<Value> Values { get; set; }
+    }
+}
